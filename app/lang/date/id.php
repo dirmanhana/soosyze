9e28e316069621072b12ab2a770a32b21a79058b
dirman <35218057+dirmanhana@users.noreply.github.com>
2025-01-03 01:39:57 +0700

    Create id.php
    indonesian translation

---
+++ app/lang/date/id.php
@@ -0,0 +1,18 @@
+
+<?php
+
+return [
+    'months'         => [
+        'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
+        'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+    ],
+    'months_short'   => [
+        'Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun',
+        'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'
+    ],
+    'weekdays'       => [
+        'Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'
+    ],
+    'weekdays_short' => [ 'min', 'sen', 'sel', 'rab', 'kam', 'jum', 'Sab' ],
+    'weekdays_min'   => [ 'mi', 'se', 'se', 'ra', 'ka', 'ju', 'sa' ],
+];
